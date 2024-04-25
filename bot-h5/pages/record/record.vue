013baddde5a0f0308ebcaf9ec96d2e125b878ee5
lhj501382150 <liuhaojie113@163.com>
2024-04-25 11:26:15 +0800

    1

--- bot-h5/pages/record/record.vue
+++ bot-h5/pages/record/record.vue
@@ -17,11 +17,11 @@
 						  <view class="row">玩法：<text class="red">{{item.artid}}</text></view>
 						  <view class="row">倍率：<text class="red">{{item.cpright}}</text></view>
 						  <view class="row">金额：<text class="red">{{item.bailmoney}}</text></view>
-						  <view class="row">中奖金额：<text class="red" v-if="item.comm >0">{{item.loss + item.bailmoney - item.comm}}</text></view>
+						  <view class="row">中奖金额：<text class="red" v-if="item.bno">{{item.loss + item.bailmoney - item.comm}}</text></view>
 						  <view class="row">下注时间：{{item.ordtime}}</view>
 					  </view>
 					  <view class="right">
-						  <view class="red" v-if="item.comm==0">未开奖</view>
+						  <view class="red" v-if="!item.bno">未开奖</view>
 						  <view v-else>
 							  <view class="red" v-if="item.loss < 0 ">未中奖</view>
 							  <view class="blue" v-else-if="item.loss==0">打和</view>
